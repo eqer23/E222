2707519bb458c78411f0b3d0ad4efdfc3395382e
eqer23 <58919050+eqer23@users.noreply.github.com>
2021-04-16 15:07:17 -0400

    current version for jetstream

--- MachLearn/my_python_dir/prediction.py
+++ MachLearn/my_python_dir/prediction.py
@@ -8,8 +8,16 @@ import json
 
 my_model = load("heartfailure.pkl")
 
+
 #endpoint points here
 def my_prediction(id):
-    return
+    dummy = np.array(id)
+    dummyT = dummy.reshape(1,-1)
+    prediction = my_model.predict(dummyT)
+    #name = class_names[prediction]
+   # name = name.tolist()
+   # name_str = json.dumps(name)
+   # string = [t_str, r_str, name_str]
+    return str(prediction[0])
 
 
